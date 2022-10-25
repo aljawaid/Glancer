f8002404bfd8df6191b03c2fbd569c287dc58d41
aljawaid <jawaid_imran@hotmail.com>
2022-10-25 10:01:43 +0100

    NEW: Add Translations

--- Locale/en_GB/translations.php
+++ Locale/en_GB/translations.php
@@ -1,5 +1,24 @@
 <?php
 return array(
   'Glancer lets you quickly jump to a project, task or comment through a mini bar at the bottom of the screen' => 'Glancer lets you quickly jump to a project, task or comment through a mini bar at the bottom of the screen',
-  'Assignee' => 'Assigned To',
+  // GLANCER FILE
+  'View:' => 'View',
+  'Task ID' => 'Task ID',
+  'Enter Task ID' => 'Enter Task ID',
+  'Comment ID' => 'Comment ID',
+  'Enter Comment ID' => 'Enter Comment ID',
+  'Project ID' => 'Project ID',
+  'Enter Project ID' => 'Enter Project ID',
+  '' => '',
+  '' => '',
+  '' => '',
+  '' => '',
+  '' => '',
+  // COMMENT SHOW FILE
+  'Created at:' => 'Created:',
+  'Updated at:' => 'Updated:',
+  'ID: ' => 'ID: ',
+  'Link' => 'Link',
+  'Edit' => 'Edit',
+  'Remove' => 'Delete',
 );
