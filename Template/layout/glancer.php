09f7f7507bdc3af43dd0c347bb57bdc870dfc8db
aljawaid <jawaid_imran@hotmail.com>
2022-10-25 10:02:57 +0100

    Cleanup Code with Translations

--- Template/layout/glancer.php
+++ Template/layout/glancer.php
@@ -1,46 +1,49 @@
 <div class="white-bar-margin"></div>
 
 <div id="GlancerBar" class="glancer-bar">
-	<div class="glancer-view"><i class="fa fa-search i-fw" aria-hidden="true"></i>View:</div>
+	<div class="glancer-view"><i class="fa fa-search i-fw" aria-hidden="true"></i><?= t('View:') ?></div>
 	<div id="SearchTaskID" class="glancer-bar-item">
 		<div class="glancer-bar-title">
-			<i class="fa fa-sticky-note i-fw" aria-hidden="true"></i>Task ID
+			<i class="fa fa-sticky-note i-fw" aria-hidden="true"></i><?= t('Task ID') ?>
 		</div>
 		<div class="glancer-bar-input">
 			<form method="get" action="<?= $this->url->dir() ?>" class="search">
             <?= $this->form->hidden('controller', array('controller' => 'TaskViewController')) ?>
             <?= $this->form->hidden('action', array('action' => 'show')) ?>
             <div class="glancer-bar-input">
-                <?= $this->form->text('task_id', array(), array(), array('placeholder="'.t('TEST OK').'"', 'pattern="[0-9.]+" title="'.t('Task number').'"'.'aria-label="'.t('Enter task id').'"'.'style=""'), 'input-addon-field border-radius') ?>
+                <?= $this->form->text('task_id', array(), array(), array('placeholder="'.t('Enter Task ID').'"', 'pattern="[0-9.]+" title="'.t('Enter Task ID').'"'.'aria-label="'.t('Enter Task ID').'"'.'style=""'), 'input-addon-field border-radius') ?>
             </div>
         	</form>
         </div>
 	</div>
 	<div id="SearchCommentID" class="glancer-bar-item">
 		<div class="glancer-bar-title">
-			<i class="fa fa-comment i-fw" aria-hidden="true"></i>Comment ID
+			<i class="fa fa-comment i-fw" aria-hidden="true"></i><?= t('Comment ID') ?>
 		</div>
 		<div class="glancer-bar-input">
-			<?php $commentID = $this->model->commentModel->getById($comment['id']); ?>
-			<form method="get" action="<?= $this->url->dir() ?>" class="search">
+			<?= $this->model->commentModel->getById($comment['id']); ?>
+			<form method="get" action="<?= $this->url->dir() ?>#comment-<?= $comment['id']; ?>" class="search">
             <?= $this->form->hidden('controller', array('controller' => 'TaskViewController')) ?>
-            <?= $this->form->hidden('action', array('action' => 'show')) ?>
+            <?= $this->form->hidden('action', array('action' => 'show','task_id' => $task['id'], 'project_id' => $task['project_id'], 'comment_id' => $comment['id'])) ?>
+
+            
+
             <div class="glancer-bar-input">
-                <?= $this->form->text($commentID, array(), array(), array('placeholder="'.t('TEST NOT OK').'"', 'pattern="[0-9.]+" title="'.t('Comment ID').'"'.'aria-label="'.t('Enter comment id').'"'.'style=""'), 'input-addon-field border-radius') ?>
+                <?= $this->form->text('comment_id', array(), array(), array('placeholder="'.t('Enter Comment ID').'"', 'pattern="[0-9.]+" title="'.t('Enter Comment ID').'"'.'aria-label="'.t('Enter Comment ID').'"'.'style=""'), 'input-addon-field border-radius') ?>
             </div>
         	</form>
 		</div>
 	</div>
 	<div id="SearchProjectID" class="glancer-bar-item">
 			<div class="glancer-bar-title">
-			<i class="fa fa-folder i-fw" aria-hidden="true"></i>Project ID
+			<i class="fa fa-folder i-fw" aria-hidden="true"></i><?= t('Project ID') ?>
 		</div>
 		<div class="glancer-bar-input">
 			<form method="get" action="<?= $this->url->dir() ?>" class="search">
             <?= $this->form->hidden('controller', array('controller' => 'BoardViewController')) ?>
             <?= $this->form->hidden('action', array('action' => 'show')) ?>
             <div class="glancer-bar-input">
-                <?= $this->form->text('project_id', array(), array(), array('placeholder="'.t('TEST OK').'"', 'pattern="[0-9.]+" title="'.t('Project number').'"'.'aria-label="'.t('Enter project id').'"'.'style=""'), 'input-addon-field border-radius') ?>
+                <?= $this->form->text('project_id', array(), array(), array('placeholder="'.t('Enter Project ID').'"', 'pattern="[0-9.]+" title="'.t('Enter Project ID').'"'.'aria-label="'.t('Enter Project ID').'"'.'style=""'), 'input-addon-field border-radius') ?>
             </div>
         	</form>
 		</div>
