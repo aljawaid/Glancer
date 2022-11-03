2aa3e57bd11d2d15fc772595be31c07f6871cf84
aljawaid <jawaid_imran@hotmail.com>
2022-11-03 04:02:25 +0000

    Update comments

--- Template/comment/show.php
+++ Template/comment/show.php
@@ -13,22 +13,23 @@
         <small class="comment-date updated-comment"><?= t('Updated at:') ?>&nbsp;
             <kbd class="comment-updated"><?= $this->dt->datetime($comment['date_modification']) ?><abbr title="Local Time">LT</abbr></kbd>
         </small>
-        <small class="comment-date comment-id"><i class="fa fa-comment-o fa-fw"></i> <?= t('ID: ') ?> <kbd class="comment-created"><?= $this->text->e($comment['id']) ?></kbd></small>
+        <small class="comment-date comment-id"><i class="fa fa-comment-o fa-fw"></i> <?= t('ID: ') ?> <kbd class="comment-created"><?= $this->text->e($comment['id']) ?></kbd>
+        </small>
     </div>
 
     <?php if (! isset($hide_actions)): ?>
     <div class="comment-actions action-comment">
         <div class="dropdown">
             <a href="#" class="dropdown-menu dropdown-menu-link-icon"><i class="fa fa-cog"></i><i class="fa fa-caret-down fa-fw"></i></a>
-            <ul>
-                <li>
+            <ul class="">
+                <li class="">
                     <?= $this->url->icon('link', t('Link'), 'TaskViewController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id']), false, '', '', $this->app->isAjax(), 'comment-'.$comment['id']) ?>
                 </li>
                 <?php if ($editable && ($this->user->isAdmin() || $this->user->isCurrentUser($comment['user_id']))): ?>
-                    <li>
+                    <li class="">
                         <?= $this->modal->medium('edit', t('Edit'), 'CommentController', 'edit', array('task_id' => $task['id'], 'project_id' => $task['project_id'], 'comment_id' => $comment['id'])) ?>
                     </li>
-                    <li>
+                    <li class="">
                         <?= $this->modal->confirm('trash-o', t('Remove'), 'CommentController', 'confirm', array('task_id' => $task['id'], 'project_id' => $task['project_id'], 'comment_id' => $comment['id'])) ?>
                     </li>
                 <?php endif ?>
