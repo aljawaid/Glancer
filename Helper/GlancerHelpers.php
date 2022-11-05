13d3791283f805172301d26b30ad8a6b98a95ba8
aljawaid <jawaid_imran@hotmail.com>
2022-11-05 13:15:38 +0000

    WIP Construct Helper to Resolve Comment Age Issue
    - Reference commit cfbf1fe
    - WIP #10

--- Helper/GlancerHelpers.php
+++ Helper/GlancerHelpers.php
@@ -12,21 +12,26 @@ class AgeHelper extends Base
             $now = time();
         }
 
+        $timestamp = $comment['date_creation'];
+
         $diff = $now - $timestamp;
 
         if ($diff < 900) {
             return t('<15m');
         }
         if ($diff < 1200) {
-            return t('<30m');
+            return t('- less than half an hour ago');
         } elseif ($diff < 3600) {
-            return t('<1h');
+            return t('- less than an hour ago');
         } elseif ($diff < 86400) {
+            // what does this mean in human terms? less than xxhrs ago?
             return '~'.t('%dh', $diff / 3600);
+            // because this goes to days
         } elseif ($diff < (86400 * 2))  {
             return '- yesterday';
         } elseif ($diff < (86400 * 5)) {
             return '- a few days ago';
+            // mins x days? what if a week is classed as 5-7 days... is it just > ($diff = (86400 * 7) && ($diff = (86400 * 9)
         } elseif ($diff = (86400 * 7)) {
             return '-  a week ago';
         } elseif ($diff < (86400 * 9)) {
