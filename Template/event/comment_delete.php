bb7b58bca64d6e89989bc698b83aa16a57c17d16
aljawaid <jawaid_imran@hotmail.com>
2022-11-05 23:38:46 +0000

    Update Activity `comment_delete.php`
    - WIP #4

--- Template/event/comment_delete.php
+++ Template/event/comment_delete.php
@@ -1,11 +1,30 @@
-<p class="activity-title">
-    <?= e('%s removed a comment on the task %s',
-        $this->text->e($author),
-        $this->url->link(t('#%d', $task['id']), 'TaskViewController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id']))
-    ) ?>
-    <small class="activity-date"><?= $this->dt->datetime($date_creation) ?></small>
-</p>
-<div class="activity-description">
-    <p class="activity-task-title"><?= $this->text->e($task['title']) ?></p>
-    <div class="markdown"><?= $this->text->markdown($comment['comment']) ?></div>
-</div>
+<div class="activity-comment-wrapper">
+    <span id="CommentSideIcon" class="comment-side-icon"><i class="fa fa-comment-o fa-fw"></i></span>
+    <p class="activity-title activity-comment-title">
+        <?= e('%s removed a comment on the task %s',
+            $this->text->e($author),
+            $this->url->link(t('#%d', $task['id']), 'TaskViewController', 'show', array('task_id' => $task['id'], 'project_id' => $task['project_id']))
+            ) ?>
+        <span class="ago">
+            <?= $this->helper->ageHelper->newAge($comment['date_creation']) ?>
+        </span>
+        <small class="activity-date activity-comment-date comment-date" title="<?= t('Date Created') ?>"><?= t('Created') ?>&nbsp;
+            <kbd class="comment-created"><?= $this->dt->datetime($date_creation) ?><abbr title="<?= t('Local Time') ?>"><?= t('LT') ?></abbr></kbd>
+        </small>
+        <small class="comment-date activity-comment-id comment-id" title="<?= t('Comment ID') ?>">
+            <i class="fa fa-comment-o fa-fw"></i> <?= t('ID ') ?> <kbd class="comment-updated"><?= $this->text->e($comment['id']) ?></kbd>
+        </small>
+    </p>
+    <div class="activity-description activity-comment-description">
+        <p class="activity-task-title activity-comment-task-title" title="<?= t('Task Title') ?>">
+            <i class="fa fa-sticky-note icon-grey" aria-hidden="true"></i> <?= $this->text->e($task['title']) ?>
+        </p>
+        <div id="JumpComment" class="comment-actions action-comment">
+            <ul class="">
+                <li class="">
+                    <?= $this->url->link(t('View Comment'), 'TaskCommentViewController', 'getCommentFromButton', array('plugin' => 'Glancer',  'projectid' => $task['project_id'], 'taskid' => $task['id'], 'commentid' => $comment['id']), false, 'comment-link') ?>
+                </li>
+            </ul>
+        </div>
+        <div class="markdown"><?= $this->text->markdown($comment['comment']) ?></div>
+    </div>
