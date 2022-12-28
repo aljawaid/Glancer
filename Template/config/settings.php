bc7b85b658bfab6a2a3aeac64de9ea1ee8f34ed5
aljawaid <10233708+aljawaid@users.noreply.github.com>
2022-12-28 22:03:59 +0000

    Remove Plugin Variables

--- Template/config/settings.php
+++ Template/config/settings.php
@@ -1,9 +1,6 @@
-<?php $pluginVersion = Kanboard\Plugin\Glancer\Plugin::getPluginVersion(); ?>
-
 <div id="GlancerSettings" class="panel glancer-settings">
-    <span class="glancer-badge-version">v<?= $pluginVersion ?></span>
     <h3 class="">
-        <span class="pp-blue" title="Glancer v<?= $pluginVersion ?>">
+        <span class="pp-blue" title="">
             <svg width="26" height="26" fill="currentColor" class="bi bi-person-workspace glancer-icon" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" >
                 <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
                 <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"/>
