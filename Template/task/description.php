77893b503374a16bcc8bd670a9821cb9eddfbb03
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-22 11:54:56 +0100

    FIX: Code Syntax - `description.php`

--- Template/task/description.php
+++ Template/task/description.php
@@ -1,7 +1,7 @@
 <details class="accordion-section">
     <summary class="accordion-title acc-desc-title">
         <i class="fa fa-file-text-o"></i><?= t('Description') ?>
-        <?php if (! empty($task['description'])): ?>
+        <?php if (!empty($task['description'])): ?>
             <span class="pp-green" title="<?= t('Complete') ?>"><i class="fa fa-check-circle"></i></span>
         <?php else: ?>
             <span class="pp-red" title="<?= t('Not complete') ?>"><i class="fa fa-times-circle"></i></span>
@@ -11,7 +11,7 @@
         <?php if (empty($task['description'])): ?>
             <div class="no-data"><?= t('No description') ?></div>
         <?php endif ?>
-        <?php if (! empty($task['description'])): ?>
+        <?php if (!empty($task['description'])): ?>
         <ul class="copy-bar">
             <?php
                 $unstrippedDescription = $this->text->markdown($task['description'], isset($is_public) && $is_public);
